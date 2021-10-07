8ade8491f9fff50724223ba587b7bb344379371c
Joseph <jxlove2020@gmail.com>
2021-10-07 10:42:22 +0900

    api 로 data 가져와서 화면에 적용시키기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
-function App() {
-  return <div>Hello</div>;
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating'
+    );
+
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => {
+              // console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                ></Movie>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
