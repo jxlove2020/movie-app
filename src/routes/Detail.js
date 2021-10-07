f5926fa1b3308ac36ff093792f1a1c6dd402fe26
Joseph <jxlove2020@gmail.com>
2021-10-07 15:29:28 +0900

    detail 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component {
   componentDidMount() {
     const { location, history } = this.props;
     if (location.state === undefined) {
-      history.push("/");
+      history.push('/');
     }
   }
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="detail__container">
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <div className="movie__data">
+            <h3 className="movie__title">{location.state.title}</h3>
+            <h5 className="movie__year">{location.state.year}</h5>
+            <ul className="movie__genres">
+              {location.state.genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{location.state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
